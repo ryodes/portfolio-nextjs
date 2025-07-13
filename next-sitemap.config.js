206fb8758c166f53ea003cf2c6f761eb68749f0c
ryodes <k2acim31@gmail.com>
2025-07-13 16:37:17 +0200

    sitemap with good url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://adelkacimi.dev',
+  siteUrl: 'https://adel-kacimi.vercel.app',
   generateRobotsTxt: true,
 };
--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Adel Kacimi | Développeur Fullstack</title>
       <link rel="icon" href="/favicon.ico" />
-      <link rel="canonical" href="https://adelkacimi.dev" />
+      <link rel="canonical" href="https://adel-kacimi.vercel.app" />
       <meta name="theme-color" content="#0f172a" />
       <meta
         name="description"
@@ -16,7 +16,7 @@ export default function Head() {
       />
       <meta property="og:image" content="/me.jpg" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://adelkacimi.dev" />
+      <meta property="og:url" content="https://adel-kacimi.vercel.app" />
     </>
   );
 }
